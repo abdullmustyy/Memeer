5d721fc78312d026258ae30a5f7e84550b8b2691
abdullmustyy <abdulmusty03@outlook.com>
2022-11-07 12:46:47 +0100

    Initial commit

---
+++ memeer-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <h1 className="font-bold text-6xl text-blue-800 absolute top-1/2 left-1/2">
+      Memeer.😏🏗️
+    </h1>
+  );
+}
+
+export default App;
